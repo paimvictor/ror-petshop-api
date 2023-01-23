1385a90a8dbafc30900f5e74957b65f3a98ec086
Victor Paim <victorspaim@hotmail.com>
2023-01-23 15:22:48 -0300

    test(cliente): Adds tests to cliente_controller

---
+++ spec/requests/cliente_controller_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+RSpec.describe "ClienteControllers", type: :request do
+  describe "GET" do
+    it "returns http success" do
+      get "/cliente"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET by id" do
+    it "returns http success" do
+      cliente = create(:cliente)
+      get "/cliente/#{cliente.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new cliente" do
+        expect {
+          post "/cliente", params: { cliente: FactoryBot.attributes_for(:cliente) }
+        }.to change(Cliente, :count).by(1)
+      end
+
+      it "returns a 201 status code" do
+        post "/cliente", params: { cliente: FactoryBot.attributes_for(:cliente) }
+        expect(response).to have_http_status(201)
+      end
+
+      it "returns the created cliente as JSON" do
+        cliente_data = FactoryBot.attributes_for(:cliente)
+        post "/cliente", params: { cliente: cliente_data }
+        json = JSON.parse(response.body)
+        expect(json["nome"]).to eq(cliente_data[:nome])
+        expect(json["email"]).to eq(cliente_data[:email])
+        expect(json["telefone"]).to eq(cliente_data[:telefone])
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        { nome: "Novo Nome" }
+      }
+
+      it "updates the requested cliente" do
+        cliente = create(:cliente)
+        put "/cliente/#{cliente.id}", params: { cliente: new_attributes }
+        cliente.reload
+        expect(cliente.nome).to eq(new_attributes[:nome])
+      end
+
+      it "returns a 200 status code" do
+        cliente = create(:cliente)
+        put "/cliente/#{cliente.id}", params: { cliente: new_attributes }
+        expect(response).to have_http_status(200)
+      end
+
+      it "returns the updated cliente as JSON" do
+        cliente = create(:cliente)
+        put "/cliente/#{cliente.id}", params: { cliente: new_attributes }
+        json = JSON.parse(response.body)
+        expect(json["nome"]).to eq(new_attributes[:nome])
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested cliente" do
+      cliente = create(:cliente)
+      expect {
+        delete "/cliente/#{cliente.id}"
+      }.to change(Cliente, :count).by(-1)
+    end
+
+    it "returns a 204 status code" do
+      cliente = create(:cliente)
+      delete "/cliente/#{cliente.id}"
+      expect(response).to have_http_status(204)
+    end
+  end
+end
