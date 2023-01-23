bc61b0e43badc818ff1970e9f96dbd3a83581487
Victor Paim <victorspaim@hotmail.com>
2023-01-23 14:52:38 -0300

    test(servico): Creates tests

---
+++ spec/factories/servicos.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :servico do
+    
+  end
+end
---
+++ spec/models/servico_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Servico, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:titulo) }
+    it { should validate_presence_of(:preco) }
+    it { should validate_presence_of(:data_agendamento) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:pet) }
+  end
+
+  describe "validates preco" do
+    it "should be valid" do
+      pet = Pet.create(nome: "Fido", especie: "dog", raca: "Golden Retriever")
+      servico = Servico.new(titulo: "Banho", preco: 50, data_agendamento: DateTime.now, pet: pet)
+      expect(servico.valid?).to be true
+    end
+  end
+end
