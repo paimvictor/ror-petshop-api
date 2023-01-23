4566e97016e0ed63d2395dec35ae138b5992b00e
Victor Paim <victorspaim@hotmail.com>
2023-01-23 15:13:03 -0300

    test: Fixes telefone factory

--- spec/factories/clientes.rb
+++ spec/factories/clientes.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :cliente do
     nome { Faker::Name.name }
     email { Faker::Internet.email }
-    telefone { Faker::PhoneNumber.phone_number }
+    telefone { "41123456789" }
   end
 end
