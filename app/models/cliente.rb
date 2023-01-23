e81992ec013070ce7356dbfd437bf773f83344ea
Victor Paim <victorspaim@hotmail.com>
2023-01-23 14:44:14 -0300

    feat(cliente): Adds validations

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,7 @@
 class Cliente < ApplicationRecord
+    has_many :pets, dependent: :destroy
+
+    validates :nome, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+    validates :telefone, presence: true, length: { is: 11 }
 end
