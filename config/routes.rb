6399cadbfb628b1c54447e10fa2a8b72b594ae0b
Victor Paim <victorspaim@hotmail.com>
2023-01-23 16:12:59 -0300

    chore: Changes the pet route to /pet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :cliente do
-    resources :pet
-  end
+  resources :cliente
+  resources :pet
   resources :servico do 
     get :search, on: :collection, constraints: {
       data_agendamento: /\d{4}-\d{2}-\d{2}/
