cb76b3c0fd816d9f5e391f3b241d85988c845d5f
Victor Paim <victorspaim@hotmail.com>
2023-01-24 15:47:53 -0300

    refactor: translates servico_spec to english

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,4 +1,7 @@
 RSpec.describe Service, type: :model do
+  let(:pet) { create(:pet) }
+  let(:valid_service) { build_stubbed(:service, pet: pet) }
+  let(:invalid_service) { build_stubbed(:service, price: -50, pet: pet) }
   describe "validations" do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:price) }
@@ -10,10 +13,6 @@ RSpec.describe Service, type: :model do
   end
 
   describe "validates price" do
-    let(:pet) { create(:pet) }
-    let(:valid_service) { build_stubbed(:service, pet: pet) }
-    let(:invalid_service) { build_stubbed(:service, price: -50, pet: pet) }
-
     it "is valid with valid attributes" do
       expect(valid_service).to be_valid
     end
