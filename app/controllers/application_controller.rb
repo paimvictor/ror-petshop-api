ab2ca8209e4c74aadc641d54aafaec95f2e34b57
Victor Paim <victorspaim@hotmail.com>
2023-01-25 09:19:51 -0300

    feat: Adds locale translation and parameter based switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    around_action :switch_locale
+
+    def switch_locale(&action)
+        # Switch to the locale sent in the params
+        # or the default locale if no locale is sent
+        # Example: http://localhost:3000/?locale=pt-BR
+        locale = params[:locale] || I18n.default_locale
+        I18n.with_locale(locale, &action)
+    end
 end
